8878de4ea3bcfd8dae48d9d7ed512f05f9e2aeea
amarbazlin <aamarbazlin@gmail.com>
2025-10-19 23:33:38 +0530

    make

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-     "./index.html",
-     "./about.html",
-     "./products.html",
-     "./sub.html",
-     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./index.html",
+    "./about.html",
+    "./menu.html",
+    "./contact.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   darkMode: 'media',
   theme: {
     extend: {
       fontFamily: {
-        'roboto-slab': ['Roboto Slab', 'serif'],
-        'lora': ['Lora', 'serif'],
+        'rowdies': ['Rowdies', 'sans-serif']
       },
     },
   },
   plugins: [],
 }
-
